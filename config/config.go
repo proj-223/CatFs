721be6d742e1005ec20a4587c35cfdc560788b5d
sheimi <sheimi.zhang@gmail.com>
2014-05-20 17:35:10 -0700

    add client pool

--- config/config.go
+++ config/config.go
@@ -1,6 +1,69 @@
 package config
 
+import (
+	"fmt"
+)
+
 type BlockServerConfig struct {
 	Port      string
 	BlockSize int64
+}
+
+type MasterConfig struct {
+	Host string
+	Port string
+}
+
+type DataServerConfig struct {
+	Host string
+	Port string
+}
+
+type MachineConfig struct {
+	Master      *MasterConfig
+	DataServers []*DataServerConfig
+}
+
+func (self *MachineConfig) MaterAddr() string {
+	return fmt.Sprintf("%s:%s", self.Master.Host, self.Master.Port)
+}
+
+func (self *MachineConfig) DataServerAddr(index int) string {
+	return fmt.Sprintf("%s:%s", self.DataServers[index].Host,
+		self.DataServers[index].Port)
+}
+
+func (self *MachineConfig) DataServerAddrs() []string {
+	var addrs []string
+	for i := range self.DataServers {
+		addrs = append(addrs, self.DataServerAddr(i))
+	}
+	return addrs
+}
+
+const (
+	DefaultHost = "localhost"
+)
+
+var (
+	DefaultMasterConfig = &MasterConfig{
+		Host: DefaultHost,
+		Port: "10000",
+	}
+	DefaultMachineConfig = &MachineConfig{
+		Master: DefaultMasterConfig,
+		DataServers: []*DataServerConfig{
+			DefaultDataServerConfig("10001"),
+			DefaultDataServerConfig("10002"),
+			DefaultDataServerConfig("10003"),
+			DefaultDataServerConfig("10004"),
+		},
+	}
+)
+
+func DefaultDataServerConfig(port string) *DataServerConfig {
+	return &DataServerConfig{
+		Host: DefaultHost,
+		Port: port,
+	}
 }
---
+++ protocols/pool.go
@@ -0,0 +1,58 @@
+package protocols
+
+import (
+	"github.com/proj-223/CatFs/config"
+)
+
+var (
+	DefaultClientPool = NewClientPool(config.DefaultMachineConfig)
+)
+
+func MasterServer() *MasterRPCClient {
+	return DefaultClientPool.MasterServer()
+}
+
+func DataServer(index int) *DataRPCClient {
+	return DefaultClientPool.DataServer(index)
+}
+
+func Close() {
+	DefaultClientPool.Close()
+}
+
+type ClientPool struct {
+	master      *MasterRPCClient
+	dataServers []*DataRPCClient
+}
+
+// Get the Master Server Client
+func (self *ClientPool) MasterServer() *MasterRPCClient {
+	return self.master
+}
+
+// Get the Data Server Client
+func (self *ClientPool) DataServer(index int) *DataRPCClient {
+	if index >= len(self.dataServers) {
+		return nil
+	}
+	return self.dataServers[index]
+}
+
+// Get the Data Server Client
+func (self *ClientPool) Close() {
+	self.master.CloseConn()
+	for _, ds := range self.dataServers {
+		ds.CloseConn()
+	}
+}
+
+// init a new Client Pool
+func NewClientPool(conf *config.MachineConfig) *ClientPool {
+	cp := new(ClientPool)
+	cp.master = NewMasterClient(conf.MaterAddr())
+	addrs := conf.DataServerAddrs()
+	for _, addr := range addrs {
+		cp.dataServers = append(cp.dataServers, NewDataClient(addr))
+	}
+	return cp
+}
