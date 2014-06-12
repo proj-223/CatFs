38d3cf17d865f0119110a47bd1b602759a176736
Rizhen Zhang <sheimi.zhang@gmail.com>
2014-06-11 22:52:43 -0700

    update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ CatFs
 =====
 
 Project for CSE223b
+
+Ubuntu setup
+===========
+
+> curl -L https://raw.githubusercontent.com/proj-223/CatFs/master/ubuntu_setup.sh | sh
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get install -y git golang mercurial
-mkdir ~/go
-export GOPATH=~/go
-export PATH=~/go/bin:$PATH
+mkdir $HOME/go
+export GOPATH=$HOME/go
+export PATH=$HOME/go/bin:$PATH
 go get github.com/proj-223/CatFs
-#cd $GOPATH/src/github.com/proj-223/CatFs && git checkout -b benchmark
-origin/benchmark && go install ./...
-echo "export GOPATH=~/go" >> ~/.bashrc
-echo "export PATH=~/go/bin:$PATH" >> ~/.bashrc
+cd $GOPATH/src/github.com/proj-223/CatFs
+git checkout -b benchmark origin/benchmark
+go install ./...
+echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
+echo "export PATH=$HOME/go/bin:$PATH" >> $HOME/.bashrc
